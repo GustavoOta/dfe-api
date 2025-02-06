3f385719162ca253ece4971033eee2d5f5e8a34a
Gustavo Ota <gustavo@gravis.com.br>
2025-02-06 10:04:48 -0300

    version.json added to check updates

--- build-release.bat
+++ build-release.bat
@@ -7,3 +7,21 @@ cargo build --release
 
 @echo Copy Release to root directory
 copy target\release\*.exe .
+
+@echo Extract version information from Cargo.toml
+for /f "tokens=2 delims== " %%i in ('findstr /r /c:"^version" Cargo.toml') do set version=%%i
+set version=%version:"=%
+
+@echo Set author information
+set author=Gustavo Ota - graviscms@gmail.com
+
+@echo Set license information
+set license=For license details, visit https://github.com/GustavoOta/dfe-api
+
+@echo Create version.json
+echo { > version.json
+echo     "name": "dfe-api", >> version.json
+echo     "version": "%version%", >> version.json
+echo     "author": "%author%", >> version.json
+echo     "license": "%license%" >> version.json
+echo } >> version.json
